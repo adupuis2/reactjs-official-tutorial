1709f8e0019908be1729dad65467e3b30b93eac5
Alain Dupuis <adupuis2@unb.ca>
2018-08-20 19:55:02 -0300

    Implemented Board constructor

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square"
-        onClick={() => this.setState({value: "X"})}>
+        onClick={() => this.setState({ value: "X" })}
+      >
         {this.state.value}
       </button>
     );
@@ -21,6 +22,13 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
     return <Square value={i} />;
   }
